d7a0fe1e882b44b9eca5e80ae9a51aed4d4938e6
Michail Pischagin <mblsha@users.sourceforge.net>
2007-04-08 08:37:14 +0000

    Fixed the SyncThread::call() method.

--- third-party/qca/support/console.cpp
+++ third-party/qca/support/console.cpp
@@ -184,11 +184,9 @@ void SyncThread::stop()
 QVariant SyncThread::call(QObject *obj, const char *method, const QVariantList &args, bool *ok)
 {
 	QMutexLocker locker(&m);
-	Q_ASSERT(QMetaObject::invokeMethod(agent, "call_do", Qt::QueuedConnection,
-		Q_ARG(QObject*, obj), Q_ARG(QByteArray, QByteArray(method)), Q_ARG(QVariantList, args)));
-	Q_UNUSED(args);
-	Q_UNUSED(method);
-	Q_UNUSED(obj);
+	bool result = QMetaObject::invokeMethod(agent, "call_do", Qt::QueuedConnection,
+		Q_ARG(QObject*, obj), Q_ARG(QByteArray, QByteArray(method)), Q_ARG(QVariantList, args));
+	Q_ASSERT(result);
 	w.wait(&m);
 	if(ok)
 		*ok = last_success;
