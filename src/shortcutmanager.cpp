3c53722fab16fc107a513fbdf7af5770f31638ff
Remko Tronçon <git@el-tramo.be>
2007-04-08 08:27:22 +0000

    Fixed typo in shortcut manager.

--- src/shortcutmanager.cpp
+++ src/shortcutmanager.cpp
@@ -10,7 +10,7 @@
 ShortcutManager::ShortcutManager() 
 {
 	// Make sure that there is at least one shortcut for sending messages
-	if (shortcuts("options.shortcuts.chat.send").isEmpty()) {
+	if (shortcuts("chat.send").isEmpty()) {
 		qWarning("Restoring chat.send shortcut");
 		QVariantList vl;
 		vl << qVariantFromValue(QKeySequence(Qt::Key_Enter)) << qVariantFromValue(QKeySequence(Qt::Key_Return));
