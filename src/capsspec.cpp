cf635419ca1538cb9e2e8bb29cc62c1f023ff0ed
Remko Tronçon <git@el-tramo.be>
2007-04-19 19:35:03 +0000

    Qt4ifying CapsSpec.

--- src/capsspec.cpp
+++ src/capsspec.cpp
@@ -92,7 +92,7 @@ CapsSpecs CapsSpec::flatten() const
 {
 	CapsSpecs l;
 	l.append(CapsSpec(node(),version(),version()));
-	QStringList exts = QStringList::split(" ",extensions());
+	QStringList exts(extensions().split(" ",QString::SkipEmptyParts));
 	for (QStringList::ConstIterator i = exts.begin(); i != exts.end(); ++i) {
 		l.append(CapsSpec(node(),version(),*i));
 	}
