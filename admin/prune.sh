c5a6b6a33fe367672bf8a6b9c4c4d8533a097c17
Remko Tronçon <git@el-tramo.be>
2007-05-07 13:40:03 +0000

    Added prune script.

---
+++ admin/prune.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+rm -rf _darcs
+rm -rf TODO
+rm -rf configure-jingle psi-jingle.qc
+rm -rf third-party/qca/qca third-party/qca/qca-openssl third-party/qca/qca-sasl third-party/qca/qca-gnupg
+rm -rf third-party/customjinglefiles.txt third-party/libjingle third-party/libjingle.new
+rm -rf admin
