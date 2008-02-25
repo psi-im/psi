e2efd6a0cd762d0334c2770c5c7c4ec844506db2
Kevin Smith <kismith@psi-im.org>
2008-02-25 10:24:39 +0000

    0.12 versions

--- src/applicationinfo.cpp
+++ src/applicationinfo.cpp
@@ -26,8 +26,8 @@
 // Should also be overridable through an optional file.
 
 #define PROG_NAME "Psi"
-#define PROG_VERSION "0.12-dev" " (" __DATE__ ")"; //CVS Builds are dated
-//#define PROG_VERSION "0.11-RC1";
+//#define PROG_VERSION "0.12-dev" " (" __DATE__ ")"; //CVS Builds are dated
+#define PROG_VERSION "0.12-RC1";
 #define PROG_CAPS_NODE "http://psi-im.org/caps";
 #define PROG_CAPS_VERSION "0.12-dev-rev1";
 #define PROG_IPC_NAME "org.psi-im.Psi"	// must not contain '\\' character on Windows
