f9e9f6c22cda217b53fb0bdc51432d8cad82d01e
Justin Karneges <justin@affinix.com>
2009-04-18 12:32:38 -0700

    zero-out jinglertpmanager earlier

--- src/psiaccount.cpp
+++ src/psiaccount.cpp
@@ -274,6 +274,7 @@ public:
 		, rcForwardServer(0)
 		, avatarFactory(0)
 		, voiceCaller(0)
+		, jingleRtpManager(0)
 		, tabManager(0)
 #ifdef GOOGLE_FT
 		, googleFTManager(0)
@@ -864,7 +865,6 @@ PsiAccount::PsiAccount(const UserAccount &acc, PsiContactList *parent, CapsRegis
 	connect(d->googleFTManager,SIGNAL(incomingFileTransfer(GoogleFileTransfer*)),SLOT(incomingGoogleFileTransfer(GoogleFileTransfer*)));
 #endif
 
-	d->jingleRtpManager = 0;
 	if(JingleRtpManager::isSupported()) {
 		d->jingleRtpManager = new JingleRtpManager(this);
 		connect(d->jingleRtpManager, SIGNAL(incomingReady()), d, SLOT(incoming_call()));
