541b38c3bbebd66a8ce1a3d5468ebdd813d33012
Justin Karneges <justin@affinix.com>
2009-05-23 10:50:50 -0700

    update prune.sh to 2009

--- admin/prune.sh
+++ admin/prune.sh
@@ -3,7 +3,5 @@
 rm -rf _darcs .darcs_boring .darcs_binaries
 rm -rf .git
 rm -rf TODO
-rm -rf configure-jingle psi-jingle.qc
-rm -rf third-party/qca/qca third-party/qca/qca-openssl third-party/qca/qca-sasl third-party/qca/qca-gnupg
-rm -rf third-party/customjinglefiles.txt third-party/libjingle third-party/libjingle.new
+#rm -rf third-party/qca/qca third-party/qca/qca-openssl third-party/qca/qca-sasl third-party/qca/qca-gnupg
 rm -rf admin
