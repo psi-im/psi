8b04fbf6e438f7f878e3e05ac86001017f8259e4
Justin Karneges <justin@affinix.com>
2009-08-03 13:35:47 -0700

    use qsound to play ringing sound

--- src/ringsound.cpp
+++ src/ringsound.cpp
@@ -1,6 +1,46 @@
 #include "ringsound.h"
 
+#include <QApplication>
 #include <QTimer>
+#include <QSound>
+#include <QFile>
+#include <QDir>
+#include <QStringList>
+#include <QProcess>
+#include "applicationinfo.h"
+#include "psioptions.h"
+#include "common.h"
+
+// adapted from common.h
+static void soundPlay2(const QString &s, QSound *sound)
+{
+	QString str = s;
+	if(str == "!beep") {
+		QApplication::beep();
+		return;
+	}
+
+	if (!QDir::isRelativePath(str)) {
+		str = ApplicationInfo::resourcesDir() + "/" + str;
+	}
+
+	if(!QFile::exists(str))
+		return;
+
+#if defined(Q_WS_WIN) || defined(Q_WS_MAC)
+	if(sound)
+		sound->play(str);
+#else
+	Q_UNUSED(sound);
+	QString player = PsiOptions::instance()->getOption("options.ui.notifications.sounds.unix-sound-player").toString();
+	if (player == "") player = soundDetectPlayer();
+	QStringList args;
+	args = QStringList::split(' ', player);
+	args += str;
+	QString prog = args.takeFirst();
+	QProcess::startDetached(prog, args);
+#endif
+}
 
 class RingSound::Private : public QObject
 {
@@ -12,15 +52,21 @@ public:
 	RingSound::Mode mode;
 	int ringCount, ringMax;
 	QTimer *t;
+	QSound *sound;
 
 	Private(RingSound *_q) :
 		QObject(_q),
-		q(_q)
+		q(_q),
+		sound(0)
 	{
 		mode = RingSound::Outgoing;
 
 		t = new QTimer(this);
 		connect(t, SIGNAL(timeout()), SLOT(t_timeout()));
+
+#if defined(Q_WS_WIN) || defined(Q_WS_MAC)
+		sound = new QSound(this);
+#endif
 	}
 
 	void start()
@@ -29,7 +75,7 @@ public:
 
 		if(mode == RingSound::Outgoing)
 		{
-			// for outgoing, start ringing in 2 seconds, for
+			// for outgoing, start ringing in 3 seconds, for
 			//   a "phone feel"
 			t->start(3000);
 		}
@@ -43,7 +89,10 @@ public:
 
 	void stop()
 	{
-		// TODO: if there is a sound playing, stop it immediately
+		// if there's a sound playing, stop it immediately.
+		// FIXME: make sound stopping work on linux
+		if(sound)
+			sound->stop();
 		t->stop();
 	}
 
@@ -57,9 +106,15 @@ private slots:
 	void t_timeout()
 	{
 		if(mode == RingSound::Outgoing)
-			printf("...ring...\n");
+		{
+			//printf("...ring...\n");
+			soundPlay2("sound/ring_outgoing.wav", sound);
+		}
 		else
-			printf("*burrriiinngg*\n");
+		{
+			//printf("*burrriiinngg*\n");
+			soundPlay2("sound/ring_incoming.wav", sound);
+		}
 
 		if(ringCount == 0 && mode == RingSound::Outgoing)
 			start_regular_timer();
