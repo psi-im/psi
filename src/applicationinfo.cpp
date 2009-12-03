88963fcc4bc8714f694ab33f61d22951602fdfda
Justin Karneges <justin@affinix.com>
2009-12-03 14:57:20 -0800

    update version to 0.15-dev

--- src/applicationinfo.cpp
+++ src/applicationinfo.cpp
@@ -26,8 +26,8 @@
 // Should also be overridable through an optional file.
 
 #define PROG_NAME "Psi"
-#define PROG_VERSION "0.14-dev" " (" __DATE__ ")"; //CVS Builds are dated
-//#define PROG_VERSION "0.14";
+#define PROG_VERSION "0.15-dev" " (" __DATE__ ")"; //CVS Builds are dated
+//#define PROG_VERSION "0.15";
 #define PROG_CAPS_NODE "http://psi-im.org/caps";
 #define PROG_CAPS_VERSION "caps-b75d8d2b25";
 #define PROG_IPC_NAME "org.psi-im.Psi"	// must not contain '\\' character on Windows
