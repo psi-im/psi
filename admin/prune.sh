fd58f0d7085f6310aa74eee88131fd814271e7f9
Justin Karneges <justin@affinix.com>
2012-08-16 19:06:53 -0700

    don't prune away the admin dir at release time

--- admin/prune.sh
+++ admin/prune.sh
@@ -3,5 +3,3 @@
 rm -rf _darcs .darcs_boring .darcs_binaries
 rm -rf .git iris/.git src/libpsi/.git
 rm -rf TODO
-#rm -rf third-party/qca/qca third-party/qca/qca-openssl third-party/qca/qca-sasl third-party/qca/qca-gnupg
-rm -rf admin
