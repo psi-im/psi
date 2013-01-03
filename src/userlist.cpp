4879eb624c2031a481d463357a8b85d5a04bcf2a
wadealer <wadealer@gmail.com>
2013-01-03 09:21:11 +0000

    some escaping at contacts tooltip

--- src/userlist.cpp
+++ src/userlist.cpp
@@ -605,7 +605,7 @@ QString UserListItem::makeBareTip(bool trim, bool doLinkify) const
 			str += QString("<div style='white-space:pre'>") + QObject::tr("Presence Error") + QString(": %1").arg(Qt::escape(err[0])) + "</div>";
 			err.pop_front();
 			foreach (QString line, err)
-				str += "<div>" + line + "</div>";
+				str += "<div>" + Qt::escape(line) + "</div>";
 		}
 
 		// status message
