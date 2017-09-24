fe56fb99c9fff0b8b641750ef70bfec3249bfe09
Sergey Ilinykh <rion4ik@gmail.com>
2017-09-24 12:42:39 +0500

    Merged crucial changes from master branch

--- admin/git_revnumber.sh
+++ admin/git_revnumber.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ref_commit=2812a0af876f47b9001fcd3a4af9ad89e2ccb1ea # change version file to 1.0
+ref_commit=4b8a3473ee1de59f84627000cba462e05f8a9b84 # change version file to 1.2
 
 cd $(dirname "$0")
 git rev-list --count ${ref_commit}..HEAD
