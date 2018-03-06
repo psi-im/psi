62ab6c31e2b0115f53ceb2511e21e25f1d978be6
Boris Pek <tehnick-8@yandex.ru>
2018-03-06 13:18:45 +0300

    Add admin script: merge_release_to_master.sh

---
+++ admin/merge_release_to_master.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "${1}" ]; then
+    echo "Error! The name of release branch is not specified!"
+    echo "Example of usage:"
+    echo "${0} release-1.x"
+    exit 1
+fi
+
+MAIN_DIR="$(realpath -s $(dirname ${0})/..)"
+cd "${MAIN_DIR}"
+
+git checkout master
+git merge "origin/${1}" --no-ff 2>&1 > /dev/null || true
+git checkout master . 2>&1 > /dev/null
+git commit -a -m "Merge remote-tracking branch 'origin/${1}'"
+
