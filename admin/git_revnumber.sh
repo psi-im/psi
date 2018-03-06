c5e55678b9b7f7b062a79394bc45cb61360161d3
Boris Pek <tehnick-8@yandex.ru>
2018-03-06 13:24:42 +0300

    Update admin script: git_revnumber.sh

--- admin/git_revnumber.sh
+++ admin/git_revnumber.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-ref_commit=4b8a3473ee1de59f84627000cba462e05f8a9b84 # change version file to 1.2
-
 cd $(dirname "$0")
-git rev-list --count ${ref_commit}..HEAD
+
+if [ "$(git describe --tags | grep '\-' | wc -l)" = "1" ]; then
+    git describe --tags | cut -d - -f 2
+else
+    echo 0
+fi
