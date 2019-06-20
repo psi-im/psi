b61c0c21d7a370b63e091c1205aab0c504106634
Vitozz <thetvg@gmail.com>
2019-06-20 20:00:44 +0300

    Added share_file.png file from httpuploadplugin to system/default

--- src/psiactionlist.cpp
+++ src/psiactionlist.cpp
@@ -439,7 +439,7 @@ void PsiActionList::Private::createChat()
         IconAction *actHistory = new IconAction(tr("Message History"), "psi/history", tr("Message History"), 0, this);
         IconAction *actCompact = new IconAction(tr("Toggle Compact/Full Size"), "psi/compact", tr("Toggle Compact/Full Size"), 0, this);
         IconAction *actActiveContacts = new IconAction (tr("Active contacts"), "psi/jabber", tr("Active contacts"), 0, this);
-        IconAction *actShareFiles = new IconAction (tr("Share Files"), "psi/filemanager", tr("Share Files"), 0, this);
+        IconAction *actShareFiles = new IconAction (tr("Share Files"), "psi/share_file", tr("Share Files"), 0, this);
 
         ActionNames actions[] = {
             { "chat_clear",  actClear  },
@@ -472,7 +472,7 @@ void PsiActionList::Private::createGroupchat()
         IconAction *actHtmlText = new IconAction(tr("Set Text Format"), "psi/text", tr("Set Text Format"), 0, this);
         IconAction *actConfigure = new IconAction(tr("Configure Room"), "psi/configure-room", tr("Configure Room"), 0, this);
         IconAction *actIcon = new IconAction(tr("Select Icon"), "psi/smile", tr("Select Icon"), 0, this);
-        IconAction *actShareFiles = new IconAction (tr("Share Files"), "psi/filemanager", tr("Share Files"), 0, this);
+        IconAction *actShareFiles = new IconAction (tr("Share Files"), "psi/share_file", tr("Share Files"), 0, this);
 
         ActionNames actions[] = {
             { "gchat_clear",  actClear  },
