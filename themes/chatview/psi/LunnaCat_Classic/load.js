8f65e193f4fba60d89a785b2eefe8639ae8efa6c
Sergey Ilinykh <rion4ik@gmail.com>
2020-04-21 19:53:59 +0300

    Added LunnaCat webkit theme

---
+++ themes/chatview/psi/LunnaCat_Classic/load.js
@@ -0,0 +1,7 @@
+srvLoader.setMetaData({
+	name: "LunnaCat Classic",
+	version: "1.2",
+	authors: ["majik <maksim.maj@gmail.com>","Sergey Ilinykh <rion4ik@gmail.com>"],
+	description: "Classic dark theme with nice background.",
+	url: "http://psi-im.org"
+});
