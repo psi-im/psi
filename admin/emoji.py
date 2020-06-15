4e3f92c68a2ebffd73fcd6e70f2b7a1e747b813a
Sergey Ilinykh <rion4ik@gmail.com>
2020-06-15 13:30:57 +0300

    Add license headers to emoji-related code

--- admin/emoji.py
+++ admin/emoji.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 
+# emoji.py - Emojis registry generator
+# Copyright (C) 2020  Sergey Ilinykh
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 import re
 import pprint
 
