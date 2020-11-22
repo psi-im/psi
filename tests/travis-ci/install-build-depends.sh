216fb4a21560d21771d2a06c8e9a11a25f68a8f3
Boris Pek <tehnick-8@yandex.ru>
2020-11-23 02:05:14 +0300

    Travis CI: update build instructions for macOS

--- tests/travis-ci/install-build-depends.sh
+++ tests/travis-ci/install-build-depends.sh
@@ -34,18 +34,17 @@ fi
 
 if [ "${TARGET}" = "macos64" ]
 then
-    HOMEBREW_NO_AUTO_UPDATE=1
-    PACKAGES="ccache \
-              qtkeychain \
-              qca \
-              minizip \
-              hunspell \
-              tidy-html5 \
-              libgpg-error \
-              libotr \
-              libsignal-protocol-c"
-    for PKG in ${PACKAGES}
-    do
-        brew ls --versions "${PKG}" > /dev/null || brew install "${PKG}"
-    done
+    export HOMEBREW_NO_AUTO_UPDATE=1
+    export PACKAGES="ccache \
+                     qtkeychain \
+                     qca \
+                     minizip \
+                     hunspell \
+                     tidy-html5 \
+                     libgpg-error \
+                     libotr \
+                     libsignal-protocol-c \
+                    "
+    brew install "${PACKAGES}"
 fi
+
