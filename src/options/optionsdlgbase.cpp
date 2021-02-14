44e390586f56e4bcde955a71ff745179ac080241
Sergey Ilinykh <rion4ik@gmail.com>
2021-02-14 18:32:31 +0300

    Added QPlainTextEdit the list of widget available for options

--- src/options/optionsdlgbase.cpp
+++ src/options/optionsdlgbase.cpp
@@ -296,6 +296,7 @@ void OptionsDlgBase::Private::createChangedMap()
     changedMap.insert("QSpinBox", SIGNAL(valueChanged(int)));
     changedMap.insert("QTimeEdit", SIGNAL(valueChanged(const QTime &)));
     changedMap.insert("QTextEdit", SIGNAL(textChanged()));
+    changedMap.insert("QPlainTextEdit", SIGNAL(textChanged()));
     changedMap.insert("QTextBrowser", SIGNAL(sourceChanged(const QUrl &)));
     changedMap.insert("QMultiLineEdit", SIGNAL(textChanged()));
     // changedMap.insert("QListBox", SIGNAL(selectionChanged()));
