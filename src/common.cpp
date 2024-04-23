68787d010705b39949280338848c182847e18572
Sergei Ilinykh <rion4ik@gmail.com>
2024-04-23 21:09:42 +0300

    Removed not needed headers

--- src/common.cpp
+++ src/common.cpp
@@ -24,11 +24,8 @@
 #endif
 #include "activity.h"
 #include "applicationinfo.h"
-#include "profiles.h"
-#include "psievent.h"
 #include "psiiconset.h"
 #include "psioptions.h"
-#include "rtparse.h"
 #include "tabdlg.h"
 #ifdef HAVE_X11
 #include "x11windowsystem.h"
