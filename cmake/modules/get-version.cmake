4061f8ac5b17ebe731687b6e52064a710879471a
Sergei Ilinykh <rion4ik@gmail.com>
2026-08-29 15:30:35 +0300

    Restore original git describe version semantics

--- cmake/modules/get-version.cmake
+++ cmake/modules/get-version.cmake
@@ -53,10 +53,7 @@ endfunction()
 
 function(obtain_git_version GIT_VERSION GIT_FULL_VERSION)
     if(EXISTS "${PROJECT_SOURCE_DIR}/\.git" AND (NOT IS_SNAPSHOT))
-        # Only application/release tags are valid version anchors. Infrastructure
-        # tags (Windows SDKs, nightly plumbing, etc.) must never become Psi's
-        # version or the base of its revision count.
-        run_git(MAIN_VER describe --tags --abbrev=0 --match "[0-9]*")
+        run_git(MAIN_VER describe --tags --abbrev=0)
         if(MAIN_VER)
             set(APP_VERSION "${MAIN_VER}" PARENT_SCOPE)
             set(${GIT_VERSION} ${MAIN_VER} PARENT_SCOPE)
